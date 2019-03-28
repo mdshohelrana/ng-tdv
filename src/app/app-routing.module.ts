fc570733eb5e2168ed73e8aa6329e715a6072117
shohel <cse_shohel@yahoo.com>
2019-03-28 10:28:36 +0600

    added app router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SimpleFormComponent } from './examples/simple-form/simple-form.component';
 import { MasterChildFormComponent } from './examples/master-child-form/master-child-form.component';
+import { CommonModule } from '@angular/common';
 
 
-export const routes: Routes = [
-  {
-      path: '',
-      redirectTo: '/simple-form',
-      pathMatch: 'full'
-  },
-  { path: 'simple-form', component: SimpleFormComponent, data: { title: 'Simple Example', fileName: 'simple-form.component.ts', folderName:'simple-form' } },
-  { path: 'master-child-form', component: MasterChildFormComponent, data: { title: 'Master Child Example', fileName: 'simple-form.component.ts', folderName:'master-child-form' } },
+export const routes: Routes = [{
+  path: '',
+  redirectTo: '/simple-form',
+  pathMatch: 'full'
+},
+{ path: 'simple-form', component: SimpleFormComponent, data: { title: 'Simple Example', fileName: 'simple-form.component.ts', folderName: 'simple-form' } },
+{ path: 'master-child-form', component: MasterChildFormComponent, data: { title: 'Master Child Example', fileName: 'simple-form.component.ts', folderName: 'master-child-form' } },
 ];
 
 @NgModule({
   declarations: [
     SimpleFormComponent
-    , MasterChildFormComponent    
+    , MasterChildFormComponent
   ],
-  imports: [RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
